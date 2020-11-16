13799afe812ab6da398f9a9e11713f663cf9ae86
Rahele Eshete <reshete@umd.edu>
2020-11-16 18:48:34 -0500

    added INST326_Group313.py

---
+++ INST326_Group313.py
@@ -0,0 +1,24 @@
+""" This is a calendar to keep people organized """
+
+class Calendar:
+    """ The person's schedule
+
+    Attributes: 
+        month (str): the month
+        day (int): the day of the moth
+        year (int): the year
+
+    """
+
+
+    def __init__(self):
+
+    def get_schedule(): 
+
+    def conflicts():
+
+    def available():
+
+        
+
+    
