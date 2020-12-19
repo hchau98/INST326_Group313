2622bac7fb5f655a9134787c18fd25e5d007cbc7
gemjetsfan <jakestark99@gmail.com>
2020-12-18 21:01:26 -0500

    All done including tests

--- schedbeta.py
+++ schedbeta.py
@@ -1,13 +1,21 @@
 from INST326_Group313 import Calendar
 from INST326_Group313 import datetoid
-
+import pandas as pd
 
 
 
 testing_cal = Calendar("test_schedule_js.csv")
-print(testing_cal.fh)
-testing_cal.remove_event(5)
+#print(testing_cal.fh)
 testing_cal.add_event("12/31/2020", "20:00", "23:59", "New Years Eve Party")
 testing_cal.remove_event(2)
-print(testing_cal.fh)
-#print(testing_cal.fh.index.values[-1])
+print(testing_cal.get_schedule("3/17/2021"))
+result_sched = {"DATE": ['3/17/2021',"3/17/2021"], 'EVENT DESCRIPTION' : ["Dads Birthday Dinner", "St Patricks Day Party"], 'START TIME' : ["18:00", "21:00"], 'END TIME': ["20:00", "23:59"], 'Date IDs' : [20210317, 20210317], "Event IDs" : [4, 5]}    
+pf = pd.DataFrame(data=result_sched)
+right_ans =[]
+for index, row in pf.iterrows():
+        right_ans.append(row)
+print(right_ans)
+print(right_ans[0].equals(testing_cal.get_schedule("3/17/2021")[0]))
+print(right_ans[1].equals(testing_cal.get_schedule("3/17/2021")[1]))
+#print(testing_cal.get_schedule("3/17/2021").equals(right_ans))   
+                  
--- test_cal.py
+++ test_cal.py
@@ -1,5 +1,6 @@
 import pytest
 import INST326_Group313 as cal
+import pandas as pd
 #Jake
 def test_jake():
     assert cal.datetoid("9/13/1999") == 19990913
@@ -8,6 +9,16 @@ def test_jake():
     
     assert cal.validatedate(["12","25", "2020"]) == True
     testing_cal = cal.Calendar("test_schedule_js.csv")
-    print("hi")
+    testing_cal.add_event("12/31/2020", "20:00", "23:59", "New Years Eve Party")
+    testing_cal.remove_event(2)
+    result = testing_cal.get_schedule("3/17/2021")
+    answer_sched = {"DATE": ['3/17/2021',"3/17/2021"], 'EVENT DESCRIPTION' : ["Dads Birthday Dinner", "St Patricks Day Party"], 'START TIME' : ["18:00", "21:00"], 'END TIME': ["20:00", "23:59"], 'Date IDs' : [20210317, 20210317], "Event IDs" : [4, 5]}    
+    pf = pd.DataFrame(data=answer_sched)
+    right_ans =[]
+    for index, row in pf.iterrows():
+            right_ans.append(row)
+    assert right_ans[0].equals(result[0]) == True
+    assert right_ans[1].equals(result[1]) == True
+   
 
    
